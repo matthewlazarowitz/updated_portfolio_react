9b21417d5cd8b2c6cb100d3bffa95846cf9c5fa6
Laz <nyys811@aol.com>
2023-08-14 13:56:17 -0400

    started working on App.js , started with imports

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Header from './Header';
+import AboutMe from './aboutMe'; 
+import Projects from './Projects';
+import './Contact.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
